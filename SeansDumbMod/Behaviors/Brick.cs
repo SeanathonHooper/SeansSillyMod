86430db5b432130661dec4c48f2b9bdb32c22cb3
Sean Hoops <50555459+SmoIBirb@users.noreply.github.com>
2024-04-23 10:13:57 -0400

    Add project files.

---
+++ SeansDumbMod/Behaviors/Brick.cs
@@ -0,0 +1,51 @@
+﻿using LethalLib.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace SeansDumbMod.Behaviors
+{
+	internal class Brick : PhysicsProp
+	{
+		public Ray brickThrowRay;
+		public RaycastHit brickHit;
+		private int brickMask = 87420;
+		public bool thrown = false;
+		//public int brickHealth = 3;
+
+
+		public override void OnHitGround()
+		{
+			base.OnHitGround();
+			thrown = false;
+			//if (brickHealth <= 0)
+			//{
+			//	Destroy(this.gameObject);
+			//}
+		}
+		public override void ItemActivate(bool used, bool buttonDown = true)
+		{ 
+			if (base.IsOwner)
+			{
+				playerHeldBy.DiscardHeldObject(placeObject: true, null, GetBrickThrowDestination());
+				//brickHealth--;
+				//thrown = true;
+			}
+		}
+		public Vector3 GetBrickThrowDestination()
+		{
+			Vector3 position = base.transform.position;
+			brickThrowRay = new Ray(playerHeldBy.gameplayCamera.transform.position, playerHeldBy.gameplayCamera.transform.forward);
+			position = ((!Physics.Raycast(brickThrowRay, out brickHit, 12f, brickMask, QueryTriggerInteraction.Ignore)) ? brickThrowRay.GetPoint(10f) : brickThrowRay.GetPoint(brickHit.distance - 0.05f));
+			brickThrowRay = new Ray(position, Vector3.down);
+			if (Physics.Raycast(brickThrowRay, out brickHit, 30f, brickMask, QueryTriggerInteraction.Ignore))
+			{
+				return brickHit.point + Vector3.up * 0.05f;
+			}
+			return brickThrowRay.GetPoint(30f);
+		}
+	}
+}
+
---
+++ SeansDumbMod/Plugin.cs
@@ -0,0 +1,77 @@
+﻿using BepInEx;
+using LethalLib.Modules;
+using SeansDumbMod.Behaviors;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace SeansDumbMod
+{
+	[BepInPlugin(GUID, NAME, VERSION)]
+	public class Plugin : BaseUnityPlugin
+	{
+		const string GUID = "seanathonhooper.seansdumbmod";
+		const string NAME = "Seans Silly Mod";
+		const string VERSION = "0.0.3";
+
+		public static Plugin instance;
+
+		void Awake()
+		{
+			instance = this;
+
+			string assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "seansdumbmod");
+			AssetBundle bundle = AssetBundle.LoadFromFile(assetDir);
+
+			Item actionFigureItem = bundle.LoadAsset<Item>("Assets/Items/ActionFigureItem/StebioActionFigureItem.asset");
+
+			NetworkPrefabs.RegisterNetworkPrefab(actionFigureItem.spawnPrefab);
+			Utilities.FixMixerGroups(actionFigureItem.spawnPrefab);
+			Items.RegisterScrap(actionFigureItem, 15, Levels.LevelTypes.All);
+			
+			Item dildoItem = bundle.LoadAsset<Item>("Assets/Items/DildoItem/DildoItem.asset");
+
+			NetworkPrefabs.RegisterNetworkPrefab(dildoItem.spawnPrefab);
+			Utilities.FixMixerGroups(dildoItem.spawnPrefab);
+			Items.RegisterScrap(dildoItem, 8, Levels.LevelTypes.All);
+
+			Item brickItem = bundle.LoadAsset<Item>("Assets/Items/BrickItem/brickItem.asset");
+
+			Brick brickProperties = brickItem.spawnPrefab.AddComponent<Brick>();
+			brickProperties.grabbable = true;
+			brickProperties.grabbableToEnemies = true;
+			brickProperties.itemProperties = brickItem;
+
+			NetworkPrefabs.RegisterNetworkPrefab(brickItem.spawnPrefab);
+			Utilities.FixMixerGroups(brickItem.spawnPrefab);
+			Items.RegisterScrap(brickItem, 33, Levels.LevelTypes.All);
+
+			Item bowlingPinItem = bundle.LoadAsset<Item>("Assets/Items/BowlingPinItem/BowlingPinItem.asset");
+
+			NetworkPrefabs.RegisterNetworkPrefab(bowlingPinItem.spawnPrefab);
+			Utilities.FixMixerGroups(bowlingPinItem.spawnPrefab);
+			Items.RegisterScrap(bowlingPinItem, 15, Levels.LevelTypes.All);
+
+			Item bowlingBallItem = bundle.LoadAsset<Item>("Assets/Items/BowlingBallItem/BowlingBallItem.asset");
+
+			//BowlingBall bowlingBallMelee = bowlingBallItem.spawnPrefab.AddComponent<BowlingBall>();
+			//bowlingBallMelee.grabbable = true;
+			//bowlingBallMelee.grabbableToEnemies = true;
+			//bowlingBallMelee.itemProperties = bowlingBallItem;
+
+
+			NetworkPrefabs.RegisterNetworkPrefab(bowlingBallItem.spawnPrefab);
+			Utilities.FixMixerGroups(bowlingBallItem.spawnPrefab);
+			Items.RegisterScrap(bowlingBallItem, 12, Levels.LevelTypes.All);
+
+			TerminalNode node = ScriptableObject.CreateInstance<TerminalNode>();
+			node.clearPreviousText = true;
+			Items.RegisterShopItem(brickItem, 0);
+			Items.RegisterShopItem(bowlingPinItem, 0);
+
+
+
+			Logger.LogInfo("Patched SeansSillyMod");
+		}
+	}
+}
